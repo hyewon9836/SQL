b3b45e7efdbeb0ace036bf7c12279cd807ce7209
hyewon9836 <74521008+hyewon9836@users.noreply.github.com>
2022-08-10 11:07:10 +0900

    Add files via upload

---
+++ HackerRank/Advanced Select/The PADS.sql
@@ -0,0 +1,7 @@
+select concat(name,'(',left(occupation,1),')')
+from occupations
+order by name, occupation;
+select concat('There are a total of ',count(occupation), ' ',lower(occupation),'s.')
+from occupations
+group by occupation
+order by count(occupation), occupation
